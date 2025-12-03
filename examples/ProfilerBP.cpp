f69ca4013f3af51db1100453c1d66428062ce9f8
Diego Renner <d.renner25@imperial.ac.uk>
2025-12-03 22:52:04 +0000

    updated and generalized BP profiler

--- examples/ProfilerBP.cpp
+++ examples/ProfilerBP.cpp
@@ -1,6 +1,7 @@
 #include "mfem.hpp"
 #include <chrono>
 
+using namespace mfem;
 int main(int argc, char *argv[]) {
 	// 1) Initialize device (e.g., CUDA)
 	mfem::Device device("cuda");
@@ -15,7 +16,7 @@ int main(int argc, char *argv[]) {
 	//std::cout << "--------------------------------------------------------\n";
 
 	std::string mesh_sizes[] = {"8", "16", "24", "32", "48"};
-  std::string operators[] = {"Mass", "Stiffness", "Helmholtz"};
+  std::string operators[] = {"Mass", "Helmholtz"};
 
   for (std::string operator_name: operators) {
    for (std::string mesh_size : mesh_sizes) {
@@ -23,8 +24,10 @@ int main(int argc, char *argv[]) {
      std::string mesh_path_prefix = "/home/dbr25/MFEM-benchmark_bckp/nektar-benchmark_mesh/cube";
      std::string mesh_path_suffix = "_mesh.msh";
    	for (int order = 1; order <= 7; ++order) {
+        std::string mesh_path =
+            mesh_path_prefix + "Hex" + mesh_size + mesh_path_suffix;
    		// 3) Load mesh and create the Finite Element space
-   		mfem::Mesh mesh(mesh_path.c_str());
+   		mfem::Mesh mesh(mesh_path);
    		mfem::H1_FECollection fec(order, mesh.Dimension());
    		mfem::FiniteElementSpace fes(&mesh, &fec);
 
@@ -34,14 +37,12 @@ int main(int argc, char *argv[]) {
        BilinearForm oper(&fes);
        if (operator_name=="Mass") {
          oper.AddDomainIntegrator(new MassIntegrator());
-       } else if (operator_name=="Stiffness") {
-         oper.AddDomainIntegrator(new DiffusionIntegrator());
        } else if (operator_name=="Helmholtz") {
          oper.AddDomainIntegrator(new MassIntegrator());
          oper.AddDomainIntegrator(new DiffusionIntegrator());
        }
-   		a.SetAssemblyLevel(mfem::AssemblyLevel::PARTIAL);
-   		a.Assemble();
+   		oper.SetAssemblyLevel(mfem::AssemblyLevel::PARTIAL);
+   		oper.Assemble();
 
    		mfem::Vector x_exact(fes.GetTrueVSize());
    		mfem::Vector b(fes.GetTrueVSize());
@@ -53,7 +54,7 @@ int main(int argc, char *argv[]) {
    		x_gf.Randomize(1); // Seed with 1 for reproducibility
    		x_gf.GetTrueDofs(x_exact);
 
-   		a.Mult(x_exact, b);
+   		oper.Mult(x_exact, b);
 
    		// 6) Set up the Preconditioned Conjugate Gradient (PCG) solver
    		//mfem::OperatorJacobiSmoother M(a, fes.GetEssentialTrueDofs()); // Diagonal Preconditioner
@@ -61,9 +62,9 @@ int main(int argc, char *argv[]) {
    		//mfem::Array<int> ess_tdof_list;
    		//fes.GetEssentialTrueDofs(ess_tdof_list);
    		mfem::OperatorJacobiSmoother M;
-   		M.SetOperator(a);
+   		M.SetOperator(oper);
    		mfem::CGSolver pcg;
-   		pcg.SetOperator(a);
+   		pcg.SetOperator(oper);
    		pcg.SetPreconditioner(M);
    		pcg.SetMaxIter(max_cg_iter);
    		pcg.SetRelTol(1e-16);
@@ -106,7 +107,7 @@ int main(int argc, char *argv[]) {
    		//          << std::setw(10) << global_dofs << " | "
    		//          << std::fixed << std::setprecision(4) << time_per_cg_iteration
    		//          << std::endl;
-      std::ofstream out_file("log_mfem_profilerBP" + operator_name + ".log", std::ios::app);
+      std::ofstream out_file("log_mfem_profilerBP_" + operator_name + ".log", std::ios::app);
    		out_file << order+1 << " "
    			<< num_elements << " "
    			<< global_dofs << " "
